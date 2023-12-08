d4456a80fe8263c790b29a76b6dfccbe9984b503
jrbanda1 <jrbanda1@stu.southtexascollege.edu>
2023-12-08 15:35:49 -0600

    base: checked

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-compnay-directory-part5/',
+  base: '/vue-company-directory-part5/',
   plugins: [vue()],
   resolve: {
     alias: {
