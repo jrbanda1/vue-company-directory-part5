f3a611a76240bf5c3bcbd2b4d9ecf12f02aca2a4
jrbanda1 <jrbanda1@stu.southtexascollege.edu>
2023-12-10 18:50:51 -0600

    vite.config changes

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vue-company-directory-part5/',
+  base: '/citp45316finalprojectfrontend/',
   plugins: [vue()],
   resolve: {
     alias: {
