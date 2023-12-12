30a8220d797138a626fca638596421c83ab1c83d
jrbanda1 <jrbanda1@stu.southtexascollege.edu>
2023-12-11 21:22:59 -0600

    revert of changes to my mistake

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/citp45316finalprojectfrontend/',
+  base: '/vue-company-directory-part5/',
   plugins: [vue()],
   resolve: {
     alias: {
